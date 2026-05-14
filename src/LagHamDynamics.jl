5b9f0145a1b2006170d4163f45135396839f7766
Camden Kilton <camdenkilton@icloud.com>
2026-05-14 19:47:13 -0400

    Started adding hybrid dispatches, need to consolidate and potentially break things

--- src/LagHamDynamics.jl
+++ src/LagHamDynamics.jl
@@ -18,38 +18,34 @@ end
 Lagrangian dynamics
 """
 
-### Solving for trajectories from normal Lagrangians:
+# Allow for different kinds of Lagrangian
+abstract type AbstractLagSys end
 
+# General Lagrangian problem
 struct LagProb{F, I, T}
     sys::F
     init::I
     tspan::T
 end
 
-function solve(prob::LagProb, solver; kwargs...) # solver specifically for LagProb struct, kwargs for step size or tolerances (optional / solver dependent)
+# Continuous Lagrangian systems are a subtype of AbstractLagSys
+struct ContinuousLagSys{L} <: AbstractLagSys
+    L::L
+end
+
+# Solve a continuous Lagrangian problem
+# prob must be a LagProb whose system type is a subtype of ContinuousLagSys
+function solve(prob::LagProb{<:ContinuousLagSys}, solver; kwargs...) # solver specifically for LagProb struct, kwargs for step size or tolerances (optional / solver dependent)
     F(x, t) = lagrangian_vec_field(prob.sys, x, t)
     return solver(F, prob.init, prob.tspan; kwargs...)
 end
 
-### Trajectories from hybrid Lagrangian systems:
+### Automatic differentiation of continuous Lagrangian systems
+struct ADLagrangian{F} <: ContinuousLagSys
+    H::F
+end
 
-# struct HybridLagSys{L, h; e = 1.0}  # is this how kwargs work with structs?
-#     L::L
-#     h::h
-#     e::e
-# end
-
-# struct LagProb{F::HybridLagSys, I, T}
-#     sys::F
-#     init::I
-#     tspan::T
-# end
-
-# function solve(prob::HybridLagSys)
-
-# end
-
-### Find the equations of motion using Euler-Lagrange equations
+# Equations of motion using Euler-Lagrange equations using ForwardDiff
 
 # dL/dq is the force
 function lagrangian_force(L::Function, q::AbstractVector, qdot::AbstractVector)
@@ -61,7 +57,7 @@ function lagrangian_momentum(L::Function, q::AbstractVector, qdot::AbstractVecto
     ForwardDiff.gradient(qdot -> L(q, qdot), qdot)
 end
 
-# find mass matrix
+# Mass matrix
 function lagrangian_mass_matrix(L::Function, q::AbstractVector, qdot::AbstractVector)
     ForwardDiff.hessian(qdot -> L(q, qdot), qdot)
 end
@@ -80,7 +76,8 @@ function lagrangian_acceleration(L::Function, q::AbstractVector, qdot::AbstractV
     M \ (F - C)
 end
 
-function lagrangian_vec_field(L::Function, x::AbstractVector, t)
+# Find the complete vector field from a Lagrangian using automatic differentiation
+function lagrangian_vec_field(L::ADLagrangian, x::AbstractVector, t)
 
     # Integer division
     n = length(x) ÷ 2
@@ -98,20 +95,103 @@ function lagrangian_vec_field(L::Function, x::AbstractVector, t)
 end
 
 
-############
+### Hybrid Lagrangian systems:
+
+struct HybridLagSys{L,H,R,E} <: AbstractLagSys
+    L::L          # Lagrangian
+    h::H          # guard/event function
+    reset::R      # reset map
+    e::E          # coefficient of restitution 
+end
+
+# Make the the reset map and coefficient of restitution optional; default to fully elastic spectral reflection
+function HybridLagSys(L, h; reset = (x,e) -> spectral_refl(x,e), e = 1.0)
+    HybridLagSys(L, h, reset, e)
+end
+
+# Solve dispatch specific to hybrid systems
+function solve(prob::LagProb{<:HybridLagSys}, solver; kwargs...)
+
+    sys = prob.sys
+
+    x = prob.init
+    t0, tf = prob.tspan
+
+    
+
+    return sol
+end
+"""
+Kinda right math, not how I want to do it tho
+    # function solve(prob::LagProb{<:HybridLagSys}, solver; kwargs...)
+
+    #     sys = prob.sys
+
+    #     x = prob.init
+    #     t0, tf = prob.tspan
+
+    #     trajectory = []
+
+    #     while t0 < tf
+
+    #         # Continuous dynamics
+    #         F(x,t) = lagrangian_vec_field(sys.L, x, t)
+
+    #         # Integrate until event
+    #         sol = solver(F, x, (t0, tf); event = sys.h, kwargs...)
+
+    #         push!(trajectory, sol)
+
+    #         # No event occurred
+    #         if terminal(sol)
+    #             break
+    #         end
+
+    #         # Apply reset map after impact
+    #         x = sys.reset(sol.x[end])
+
+    #         # Restart after event time
+    #         t0 = sol.t[end]
+    #     end
+
+    #     return trajectory
+    # end
+"""
+
+
+# Default reset map: spectral reflection with coefficient of restitution
+function spectral_refl(x, e)
+
+    n = length(x) ÷ 2
+
+    q = x[1:n]          # positions
+    qdot = x[n+1:end]   # velocities
+
+    # positions unchanged, velocities reflected
+
+
+
+
+    return vcat(q, qdot)
+end
+
+
+################################
+################################
 """
 Hamiltonian dynamics
    q̇ =  ∂H/∂p
    -ṗ = ∂H/∂q
 """
 
+# Define a general Hamiltonian problem
 struct HamProb{H, I, T}
     sys::H   # Hamiltonian
     init::I   # Initial condition
     tspan::T   # Time span
 end
 
-# Solver dispatch specifically for HamProb struct
+# Solve a Hamiltonian problem
 function solve(prob::HamProb, solver; kwargs...)
     
     # Create the vector field from the hamiltonian
@@ -121,6 +201,7 @@ function solve(prob::HamProb, solver; kwargs...)
     return solver(F, prob.init, prob.tspan; kwargs...)
 end
 
+# Find the vector field from Hamilton's equations
 function hamiltonian_vec_field(Hsys::AbstractHamiltonian, x::AbstractVector, t)
 
     n = length(x) ÷ 2
@@ -129,7 +210,7 @@ function hamiltonian_vec_field(Hsys::AbstractHamiltonian, x::AbstractVector, t)
     q = x[1:n]
     p = x[n+1:end]
 
-    # Gradient of H with respect to state vector x = [q; p]
+    # Gradient of H with respect to state vector x = [q; p], this is where multiple dispatch takes care of different kind of hams
     gradH = hamiltonian_gradient(Hsys, x)
 
     dqdt = gradH[n+1:end]      # ∂H/∂p
@@ -139,7 +220,6 @@ function hamiltonian_vec_field(Hsys::AbstractHamiltonian, x::AbstractVector, t)
     return vcat(dqdt, dpdt)
 end
 
-
 ### Allow for different representations of the Hamiltonian
 abstract type AbstractHamiltonian end
 
@@ -148,7 +228,7 @@ struct ADHamiltonian{F} <: AbstractHamiltonian
     H::F
 end
 
-# dispatch to find the gradient of Hamiltonians that can be differentiated using ForwardDiff
+# Find the gradient of Hamiltonians that can be differentiated using ForwardDiff
 function hamiltonian_gradient(Hsys::ADHamiltonian, x)
     ForwardDiff.gradient(Hsys.H, x)
 end
@@ -184,6 +264,5 @@ function hamiltonian_gradient(Hsys::FDHamiltonian, x)
 
     return grad
 end
-
 
 # Could add another dispatch method to better deal with interpolating data style hams
