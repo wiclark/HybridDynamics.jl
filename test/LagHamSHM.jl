7f9eb770d4d3d3800482b0aee26c414ef1724827
Camden Kilton <camdenkilton@icloud.com>
2026-05-14 14:38:24 -0400

    Fixed nonautonomous requirement and added hamiltonian infrastructure

--- test/LagHamSHM.jl
+++ test/LagHamSHM.jl
@@ -1,18 +1,25 @@
 # testing
 
 using HybridDynamics
+using LaTeXStrings
 import Plots as plt
+
+init = [1.,0.];
+tspan = (0., 4.);
 
 function L(q,v)
 	1/2 * v[1]^2 - q[1]^2
 end;
-
-init = [1.,0.];
-tspan = (0., 3.);
 
 prob = HybridDynamics.LagProb(L, init, tspan)
 s = HybridDynamics.solve(prob, HybridDynamics.forward_euler; dt=0.01)
 
 q = [x[1] for x in s[2]]
 v = [x[2] for x in s[2]]
-plt.plot(q, v)
+p1 = plt.plot(q, v, title = "Harmonic osc. phase portrait via Euler-Lagrange", label = "", xlabel = L"q", ylabel = L"\dot{q}")
+display(p1)
+
+
+function H(q,v)
+	1/2 * v[1]^2 + q[1]^2
+end;
