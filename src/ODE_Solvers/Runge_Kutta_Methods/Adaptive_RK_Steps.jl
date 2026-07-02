40e70f803ac884ec475ddb461d3de8971832502f
DSmifh <davidsmith52314@gmail.com>
2026-07-02 16:01:56 -0400

    Fixed Adaptive solvers returns to be uniform and correct

--- src/ODE_Solvers/Runge_Kutta_Methods/Adaptive_RK_Steps.jl
+++ src/ODE_Solvers/Runge_Kutta_Methods/Adaptive_RK_Steps.jl
@@ -16,13 +16,10 @@ function take_step(solver::AdaptiveRK, prob::AbstractHybridProblem, f, xₖ, t�
 
     #Take adaptive step (passes tf to prevent overshooting)
     #Disable adaptive loop to get the true midpoint. Without it it would reset the loop and ruin or previous adaptivity
-    ######
-    ### WC: Why was adaptive=false here?
-    ######
-    x_predict, dt_used, dt_next = compute_step(solver, f, xₖ, Δt, tₖ, tf, sys, tol)
+    x_predict, Δt, dt_next = compute_step(solver, f, xₖ, Δt, tₖ, tf, sys, tol)
     # Get midpoint for quad guard check
     # For a half-step the local ceiling is just midpoint of time 
-    x_mid, _, _ = compute_step(solver, f, xₖ, dt_used / 2.0, tₖ, tf, sys, tol; adaptive=false)
+    x_mid, _, _ = compute_step(solver, f, xₖ, Δt / 2.0, tₖ, tf, sys, tol; adaptive=false)
 
     if check
         # Evaluate guards
@@ -30,11 +27,11 @@ function take_step(solver::AdaptiveRK, prob::AbstractHybridProblem, f, xₖ, t�
         h_mid  = guard(sys, x_mid)
         h_next = guard(sys, x_predict)
         # Use cross guard check
-        eventtrigger, t_root, _ = crossed_guard(sys, h_now, h_mid, h_next, tₖ, tₖ + dt_used / 2.0, tₖ + dt_used; tol=tol, direction=guard_direction)
+        eventtrigger, t_root, _ = crossed_guard(sys, h_now, h_mid, h_next, tₖ, tₖ + Δt / 2.0, tₖ + Δt; tol=tol, direction=guard_direction)
 
-        return x_predict, eventtrigger, t_root, dt_used, dt_next
+        return x_predict, eventtrigger, t_root, Δt, dt_next
     else
-        return x_predict, false, NaN, Δt, Δt
+        return x_predict, false, NaN, Δt, dt_next
     end
 end
 
