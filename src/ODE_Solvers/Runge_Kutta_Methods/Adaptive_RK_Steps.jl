7a049617c44255963c1cce5c5c5ef7f9d9bfc1ba
DSmifh <davidsmith52314@gmail.com>
2026-07-04 08:57:58 -0400

    Unified RK solvers and Logic.

--- src/ODE_Solvers/Runge_Kutta_Methods/Adaptive_RK_Steps.jl
+++ src/ODE_Solvers/Runge_Kutta_Methods/Adaptive_RK_Steps.jl
@@ -6,8 +6,8 @@ struct RK23 <: AdaptiveRK end
 
 #Adaptive step methods
 #Helper function to take the step via multiple dispatch
-compute_step(::RK23, f, x, Δt, t, tf, sys, tol; adaptive=true) = rk_23_step(f, x, Δt, t, tf, sys, tol; adaptive=adaptive)
-compute_step(::RK45, f, x, Δt, t, tf, sys, tol; adaptive=true) = rk_45_step(f, x, Δt, t, tf, sys, tol; adaptive=adaptive)
+compute_step(::RK23, f, x, Δt, t, tf, sys, tol; adaptive=true) = rk_23_step(f, x, Δt, t, tf, sys, tol)
+compute_step(::RK45, f, x, Δt, t, tf, sys, tol; adaptive=true) = rk_45_step(f, x, Δt, t, tf, sys, tol)
 
 function take_step(solver::AdaptiveRK, prob::AbstractHybridProblem, f, xₖ, tₖ, Δt, tol, sol, stepper::AbstractODESolver=ModifiedMidpoint();
         check=true, guard_direction=default_guard_direction(prob.sys))
@@ -15,19 +15,26 @@ function take_step(solver::AdaptiveRK, prob::AbstractHybridProblem, f, xₖ, t�
     tf = prob.tspan[2] #terminal time
 
     #Take adaptive step (passes tf to prevent overshooting)
-    #Disable adaptive loop to get the true midpoint. Without it it would reset the loop and ruin or previous adaptivity
     x_predict, Δt, dt_next = compute_step(solver, f, xₖ, Δt, tₖ, tf, sys, tol)
-    # Get midpoint for quad guard check
-    # For a half-step the local ceiling is just midpoint of time 
-    x_mid, _, _ = compute_step(solver, f, xₖ, Δt / 2.0, tₖ, tf, sys, tol; adaptive=false)
 
     if check
         # Evaluate guards
         h_now  = guard(sys, xₖ)
-        h_mid  = guard(sys, x_mid)
         h_next = guard(sys, x_predict)
+
+        idx = max(1, length(sol.x) - 1)
+        t_prev = sol.t[idx]
+        x_prev = sol.x[idx]
+        h_prev = guard(sys, x_prev)
         # Use cross guard check
-        eventtrigger, t_root, _ = crossed_guard(sys, h_now, h_mid, h_next, tₖ, tₖ + Δt / 2.0, tₖ + Δt; tol=tol, direction=guard_direction)
+        eventtrigger, t_root, _ = crossed_guard(sys, h_prev, h_now, h_next, t_prev, tₖ, tₖ + Δt; tol=tol, direction=guard_direction)
+
+        if eventtrigger
+            if (t_root - tₖ) < (1e-4 * Δt)
+                eventtrigger = false
+                t_root = tₖ + Δt
+            end
+        end
 
         return x_predict, eventtrigger, t_root, Δt, dt_next
     else
@@ -48,42 +55,36 @@ function updated_step(LTE::AbstractFloat, tol::AbstractFloat, Δt::AbstractFloat
 end
 
 # Runge-Kutta 23
-function rk_23_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat, tf::AbstractFloat, sys, tol; adaptive=true)
+function rk_23_step(f::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat, tf::AbstractFloat, sys, tol)
     # As this is an adaptive step solver, Δt is the step size from the previous step
     # As the step size is not of fixed size, we specify the terminal time, tf, of the problem
-    dt = minimum([Δt, tf-t])
+    dt_step = minimum([Δt, tf-t])
 
     #Intialization to make sure they exist
-    z2 = z; z3 = z
+    x2 = xₖ; x3 = xₖ
 
     # Loop through to find an acceptable step
     while true
-        h_now = guard(sys, z)
+        h_now = guard(sys, xₖ)
         # Compute the two predictions and their difference
-        k1 = f(z, t)
+        k1 = f(xₖ, t)
 
-        z2 = z + dt*k1
-        k2 = f(z2, t+dt)
-        h2 = guard(sys, z2)
+        x2 = xₖ + dt_step*k1
+        k2 = f(x2, t+dt_step)
+        h2 = guard(sys, x2)
 
-        z3 = z + dt/4*(k1+k2)
-        k3 = f(z3, t+dt/2)
-        h3 = guard(sys, z3)
+        x3 = xₖ + dt_step/4*(k1+k2)
+        k3 = f(x3, t+dt_step/2)
+        h3 = guard(sys, x3)
 
-        z1_3 = z + dt*(1/6*k1+1/6*k2+2/3*k3)
-        z1_2 = z + dt*(1/2*k1+1/2*k2)
+        x1_3 = xₖ + dt_step*(1/6*k1+1/6*k2+2/3*k3)
+        x_predict = xₖ + dt_step*(1/2*k1+1/2*k2)
 
-        #used to find true midpoint for quad guard check. 
-        #This prevents solver from spinning up a whole new adaptive loop so x_mid is truly in the mid which is what we want for quad matrix. 
-        if !adaptive
-            return z1_2, dt, dt
-        end
-
-        LTE = norm(z1_2 - z1_3)
+        LTE = norm(x1_3 - x_predict)
         # Reject or accept?
-        dt_new = updated_step(LTE, tol, dt, 3)
+        dt_next = updated_step(LTE, tol, dt_step, 3)
         
-        h_end = guard(sys, z1_2)
+        h_end = guard(sys, x_predict)
 
         #did any intermediate stage cross guard?
         stage_crossed = (h_now * h2 < 0) || (h_now * h3 < 0)
@@ -92,92 +93,85 @@ function rk_23_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::Abstra
 
         #if we crossed inside step but missed at end, force rejection
         if stage_crossed && end_missed
-            dt = dt / 2.0 #force smaller step
+            dt_step = dt_step / 2.0 #force smaller step
             continue
         end
 
         if LTE < tol
-            return z1_2, dt, dt_new
+            return x_predict, dt_step, dt_next
         else
             # We reject and repeat the loop with an updated step
-            dt = dt_new
+            dt_step = dt_next
         end
-        if dt < 1e-12
+        if dt_step < 1e-12
             @warn "Step size has decreased below 1e-12"
-            return z1_2, dt, dt_new #force break to avoid looping forever
+            return x_predict, dt_step, dt_next #force break to avoid looping forever
         end
     end
 end
 
 # Runge-Kutta 45
-function rk_45_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat, tf::AbstractFloat, sys, tol; adaptive=true)
+function rk_45_step(f::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat, tf::AbstractFloat, sys, tol)
     # As the step size is not of fixed size, we specify the terminal time, tf, of the problem
-    dt = minimum([Δt, tf-t])
+    dt_step = minimum([Δt, tf-t])
 
     #Initialization to make them exist
-    z2 = z; z3 = z; z4 = z; z5 = z; z6 = z
+    x2 = xₖ; x3 = xₖ; x4 = xₖ; x5 = xₖ; x6 = xₖ
 
     # Loop through to find an acceptable step
     while true
-        h_now = guard(sys, z)
+        h_now = guard(sys, xₖ)
         # Compute the two predictions and their difference
-        k1 = f(z, t)
+        k1 = f(xₖ, t)
 
-        z2 = z + dt*1/5*k1
-        k2 = f(z2, t+dt*1/5)
-        h2 = guard(sys, z2)
+        x2 = xₖ + dt_step*1/5*k1
+        k2 = f(x2, t+dt_step*1/5)
+        h2 = guard(sys, x2)
 
-        z3 = z + dt*(3/40*k1 + 9/40*k2)
-        k3 = f(z3, t+dt*3/10)
-        h3 = guard(sys, z3)
+        x3 = xₖ + dt_step*(3/40*k1 + 9/40*k2)
+        k3 = f(x3, t+dt_step*3/10)
+        h3 = guard(sys, x3)
 
-        z4 = z + dt*(44/45*k1 - 56/15*k2 + 32/9*k3)
-        k4 = f(z4, t+dt*4/5)
-        h4 = guard(sys, z4)
+        x4 = xₖ + dt_step*(44/45*k1 - 56/15*k2 + 32/9*k3)
+        k4 = f(x4, t+dt_step*4/5)
+        h4 = guard(sys, x4)
 
-        z5 = z + dt*(19372/6561*k1 - 25360/2187*k2 + 64448/6561*k3 - 212/729*k4)
-        k5 = f(z5,t+dt*8/9)
-        h5 = guard(sys, z5)
+        x5 = xₖ + dt_step*(19372/6561*k1 - 25360/2187*k2 + 64448/6561*k3 - 212/729*k4)
+        k5 = f(x5,t+dt_step*8/9)
+        h5 = guard(sys, x5)
 
-        z6 = z + dt*(9017/3168*k1 - 355/33*k2 + 46732/5247*k3 + 49/176*k4 - 5105/18656*k5)
-        k6 = f(z6, t+dt)
-        h6 = guard(sys, z6)
+        x6 = xₖ + dt_step*(9017/3168*k1 - 355/33*k2 + 46732/5247*k3 + 49/176*k4 - 5105/18656*k5)
+        k6 = f(x6, t+dt_step)
+        h6 = guard(sys, x6)
 
-        k7 = f(z + dt*(35/384*k1 + 0*k2 + 500/1113*k3 + 125/192*k4 - 2187/6784*k5 + 11/84*k6), t+dt)
+        k7 = f(xₖ + dt_step*(35/384*k1 + 0*k2 + 500/1113*k3 + 125/192*k4 - 2187/6784*k5 + 11/84*k6), t+dt_step)
 
         # The two updates
-        z1_4 = z + dt*k7
-        z1_5 = z + dt*(5179/57600*k1 + 0*k2 + 7571/16695*k3 + 393/640*k4 - 92097/339200*k5 + 187/2100*k6 + 1/40*k7)
+        x_predict = xₖ + dt_step*k7
+        x1_5 = xₖ + dt_step*(5179/57600*k1 + 0*k2 + 7571/16695*k3 + 393/640*k4 - 92097/339200*k5 + 187/2100*k6 + 1/40*k7)
 
-        #used to find true midpoint for quad guard check. 
-        #This prevents solver from spinning up a whole new adaptive loop so x_mid is truly in the mid which is what we want for quad matrix. 
-        if !adaptive
-            return z1_4, dt, dt
-        end
-
-        LTE = norm(z1_4 - z1_5)
+        LTE = norm(x_predict - x1_5)
         # Reject or accept?
-        dt_new = updated_step(LTE, tol, dt, 5)
+        dt_next = updated_step(LTE, tol, dt_step, 5)
         
-        h_end = guard(sys, z1_4)
+        h_end = guard(sys, x_predict)
 
         stage_crossed = (h_now * h2 < 0) || (h_now * h3 < 0) || (h_now * h4 < 0) || (h_now * h5 < 0) || (h_now * h6 < 0)
         end_missed = (h_now * h_end > 0)
 
         if stage_crossed && end_missed
-            dt = dt / 2.0 #force smaller step
+            dt_step = dt_step / 2.0 #force smaller step
             continue
         end
         
-
         if LTE < tol
-            return z1_4, dt, dt_new
+            return x_predict, dt_step, dt_next
         else
-            dt = dt_new
+            dt_step = dt_next
         end
-        if dt < 1e-12
+        if dt_step < 1e-12
             @warn "Step size has decreased below 1e-12"
-            return z1_4, dt, dt_new
+            return x_predict, dt_step, dt_next
         end
     end
 end
--- src/ODE_Solvers/Runge_Kutta_Methods/Fixed_RK_Steps.jl
+++ src/ODE_Solvers/Runge_Kutta_Methods/Fixed_RK_Steps.jl
@@ -10,11 +10,11 @@ struct ImplicitEuler <: FixedRK end
 #Single take_step for RK methods
 #Fixed step methods 
 #Helper function to take the step using multiple dispatch.
-compute_step(::ForwardEuler, f, x, Δt, t) = forward_euler_step(f, x, Δt, t)
-compute_step(::ModifiedTrap, f, x, Δt, t) = modified_trap_step(f, x, Δt, t)
-compute_step(::ModifiedMidpoint, f, x, Δt, t) = modified_midpoint_step(f, x, Δt, t)
-compute_step(::RichardsonExtrapolation, f, x, Δt, t) = richardson_step(f, x, Δt, t)
-compute_step(::RK4, f, x, Δt, t) = rk_4_step(f, x, Δt, t)
+compute_step(::ForwardEuler, f, xₖ, Δt, t) = forward_euler_step(f, xₖ, Δt, t)
+compute_step(::ModifiedTrap, f, xₖ, Δt, t) = modified_trap_step(f, xₖ, Δt, t)
+compute_step(::ModifiedMidpoint, f, xₖ, Δt, t) = modified_midpoint_step(f, xₖ, Δt, t)
+compute_step(::RichardsonExtrapolation, f, xₖ, Δt, t) = richardson_step(f, xₖ, Δt, t)
+compute_step(::RK4, f, xₖ, Δt, t) = rk_4_step(f, xₖ, Δt, t)
 
 #Note sol is not used, we do this to make using the function easier. We would need an if/else statement everytime we use this function without it
 function take_step(solver::FixedRK, prob::AbstractHybridProblem, f, xₖ, tₖ, Δt, tol, sol, stepper::AbstractODESolver=ModifiedMidpoint(); 
@@ -22,15 +22,26 @@ function take_step(solver::FixedRK, prob::AbstractHybridProblem, f, xₖ, tₖ, 
         
     sys = prob.sys
     x_predict = compute_step(solver, f, xₖ, Δt, tₖ)
-    x_mid     = compute_step(solver, f, xₖ, Δt / 2.0, tₖ)
+    
     if check
         #Evaluate Guards
         h_now  = guard(sys, xₖ)
-        h_mid  = guard(sys, x_mid)
         h_next = guard(sys, x_predict)
 
+        idx = max(1, length(sol.x) - 1)
+        t_prev = sol.t[idx]
+        x_prev = sol.x[idx]
+        h_prev = guard(sys, x_prev)
+
         #Use cross guard check
-        eventtrigger, t_root, _ = crossed_guard(sys, h_now, h_mid, h_next, tₖ, tₖ + Δt / 2.0, tₖ + Δt; tol=tol, direction=guard_direction)
+        eventtrigger, t_root, _ = crossed_guard(sys, h_prev, h_now, h_next, t_prev, tₖ, tₖ + Δt; tol=tol, direction=guard_direction)
+
+        if eventtrigger
+            if (t_root - tₖ) < (1e-4 * Δt)
+                eventtrigger = false
+                t_root = tₖ + Δt
+            end
+        end
 
         return x_predict, eventtrigger, t_root, Δt, Δt
     else
@@ -39,44 +50,44 @@ function take_step(solver::FixedRK, prob::AbstractHybridProblem, f, xₖ, tₖ, 
 end
 
 # Forward Euler
-function forward_euler_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
-    return z .+ Δt*f(z, t)
+function forward_euler_step(f::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
+    return xₖ .+ Δt*f(xₖ, t)
 end
 
 # Modified (fully explicit) Trapezoid Rule
-function modified_trap_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
-    z_guess = z .+ Δt*f(z,t)
-    return z .+ 1/2*Δt*( f(z, t) + f(z_guess, t+Δt) )
+function modified_trap_step(f::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
+    x_guess = xₖ .+ Δt*f(xₖ, t)
+    return xₖ .+ 1/2*Δt*(f(xₖ, t) + f(x_guess, t+Δt))
 end
 
 # Modified (fully explicit) Midpoint Rule
-function modified_midpoint_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
-    z_guess = z .+ Δt/2*f(z, t)
-    return z .+ Δt*f(z_guess, t+Δt/2)
+function modified_midpoint_step(f::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
+    x_guess = xₖ .+ Δt/2*f(xₖ, t)
+    return xₖ .+ Δt*f(x_guess, t+Δt/2)
 end
 
 #Classic Runge-Kutta 4
-function rk_4_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
-    k1 = f(z,t)
-    k2 = f(z + Δt/2 * k1, t + Δt/2)
-    k3 = f(z + Δt/2 * k2, t + Δt/2)
-    k4 = f(z + Δt * k3, t + Δt)
+function rk_4_step(f::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
+    k1 = f(xₖ,t)
+    k2 = f(xₖ + Δt/2 * k1, t + Δt/2)
+    k3 = f(xₖ + Δt/2 * k2, t + Δt/2)
+    k4 = f(xₖ + Δt * k3, t + Δt)
 
     #Shouldnt need to check guard in these inner stages here as no adpative step size.
 
-    return z + Δt/6 * (k1 + 2*k2 + 2*k3 + k4)
+    return xₖ + Δt/6 * (k1 + 2*k2 + 2*k3 + k4)
 end
 
 #Richardson Extrapolation based on modifed midpoint from Wiki
-function richardson_step(f::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
+function richardson_step(f::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
     #Take one full step size h
-    z1 = modified_midpoint_step(f,z,Δt,t)
+    x1 = modified_midpoint_step(f,xₖ,Δt,t)
 
     #Take two smaller steps of h/2
     Δt_half = Δt / 2
-    z_half = modified_midpoint_step(f, z, Δt_half, t)
-    z2 = modified_midpoint_step(f, z_half, Δt_half, t+Δt_half)
+    x_half = modified_midpoint_step(f, xₖ, Δt_half, t)
+    x2 = modified_midpoint_step(f, x_half, Δt_half, t+Δt_half)
 
     #Extrapolate to get rid of lower order error
-    return (4 .* z2 .- z1) ./ 3.0
+    return (4 .* x2 .- x1) ./ 3.0
 end
