fd7947c41218d2e4e8298cdf0a88f6d33a32e04c
DSmifh <davidsmith52314@gmail.com>
2026-07-04 09:23:05 -0400

    Tweaking Stochastic and Bonus solvers

--- src/ODE_Solvers/Stochastic_Methods/Fixed_SDE_Steps.jl
+++ src/ODE_Solvers/Stochastic_Methods/Fixed_SDE_Steps.jl
@@ -6,22 +6,32 @@ struct EulerMaruyama <: STO end
 # Single take_step for stochastic methods
 # Fixed step methods
 # Helper function to take the step using multiple dispatch
-compute_step(::EulerMaruyama, f, g, x, Δt, t) = euler_maruyama_step(f, g, x, Δt, t)
+compute_step(::EulerMaruyama, f, g, xₖ, Δt, t) = euler_maruyama_step(f, g, xₖ, Δt, t)
 
 # Note sol is not used, we do this to make using the function easier.
-function take_step(solver::STO, prob::prob{S, I, T}, f, g, xₖ, tₖ, Δt, tol, sol; check=true, guard_direction = default_guard_direction(prob.sys)) where {S<:StochasticSystem, I, T}
+function take_step(solver::STO, prob::prob{S, I, T}, f, g, xₖ, tₖ, Δt, tol, sol; check=true, guard_direction = default_guard_direction(prob.sys)) where {S<:StochasticSystem, I, T}    
     sys = prob.sys
     x_predict = compute_step(solver, f, g, xₖ, Δt, tₖ)
-    x_mid     = compute_step(solver, f, g, xₖ, Δt/2.0, tₖ)
 
     if check
         # Evaluate Guards
         h_now  = guard(sys, xₖ)
-        h_mid  = guard(sys, x_mid)
         h_next = guard(sys, x_predict)
 
+        idx = max(1, length(sol.x) - 1)
+        t_prev = sol.t[idx]
+        x_prev = sol.x[idx]
+        h_prev = guard(sys, x_prev)
+
         #Use cross guard check
-        eventtrigger, t_root, _ = crossed_guard(sys, h_now, h_mid, h_next, tₖ, tₖ + Δt / 2.0, tₖ + Δt; tol=tol, direction=guard_direction)
+        eventtrigger, t_root, _ = crossed_guard(sys, h_prev, h_now, h_next, t_prev, tₖ, tₖ + Δt; tol=tol, direction=guard_direction)
+
+        if eventtrigger
+            if (t_root - tₖ) < (1e-4 * Δt)
+                eventtrigger = false
+                t_root = tₖ + Δt
+            end
+        end
 
         return x_predict, eventtrigger, t_root, Δt, Δt
     else
@@ -29,9 +39,9 @@ function take_step(solver::STO, prob::prob{S, I, T}, f, g, xₖ, tₖ, Δt, tol,
     end
 end
 
-function euler_maruyama_step(f::Function, g::Function, z::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
+function euler_maruyama_step(f::Function, g::Function, xₖ::AbstractArray, Δt::AbstractFloat, t::AbstractFloat)
     # The state and diffusion term
-    fₖ, gₖ = f(z, t), g(z, t)
+    fₖ, gₖ = f(xₖ, t), g(xₖ, t)    
     # Check sizes
     if length(fₖ) ≠ size(gₖ)[1]
         @warn "Dimensions of drift and diffusion are not compatable"
