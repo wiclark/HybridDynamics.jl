a90433a898d64bcdcdd7f99bbc9dcf4f5808a608
Camden Kilton <camdenkilton@icloud.com>
2026-07-07 21:46:34 -0400

    Added api page

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,12 @@ makedocs(
     sitename = "HybridDynamics",
     format = Documenter.HTML(),
     modules = [HybridDynamics],
-    checkdocs = :none,              # Remove when documentation is written
-    remotes = nothing               # Remove when the git repo is public
+    # checkdocs = :none,              # Remove when...documentation is written?
+    # remotes = nothing,               # Remove when...the git repo is public?
+    pages = [
+        "Home" => "index.md",
+        "API Reference" => "api.md",
+    ],
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
