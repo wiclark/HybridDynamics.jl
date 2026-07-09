fd8fad6ac9f9f451f80f7130cfcb6770fbcdb171
Camden Kilton <camdenkilton@icloud.com>
2026-07-08 20:42:38 -0400

    Documentation kinda works

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,21 @@
 using Documenter
 using HybridDynamics
 
+# Automatically populate index.md
+write(joinpath(@__DIR__, "src", "index.md"),
+      read(joinpath(".", "README.md"), String))
+
+# Make the build
 makedocs(
     sitename = "HybridDynamics",
-    format = Documenter.HTML(),
+    format = Documenter.HTML(mathengine = Documenter.MathJax3()),  # The mathengine should already be MathJax
     modules = [HybridDynamics],
-    # checkdocs = :none,              # Remove when...documentation is written?
+    checkdocs = :none,              # Remove when...documentation is written?
     # remotes = nothing,               # Remove when...the git repo is public?
     pages = [
         "Home" => "index.md",
         "API Reference" => "api.md",
+        "Change Log" => "changelog.md"
     ],
 )
 
--- docs/src/api.md
+++ docs/src/api.md
@@ -1,5 +1,43 @@
 # API Reference
 
+```@docs
+HybridDynamics.solve
+```
+
+# Systems
+
+### General
 ```@autodocs
 Modules = [HybridDynamics]
+Pages = ["Systems/General.jl"]
+```
+
+### Mechanical
+```@autodocs
+Modules = [HybridDynamics]
+Pages = ["Systems/Mechanical.jl"]
+```
+
+### Nonholonomic
+```@autodocs
+Modules = [HybridDynamics]
+Pages = ["Systems/Nonholonomic.jl"]
+```
+
+### Stochastic
+```@autodocs
+Modules = [HybridDynamics]
+Pages = ["Systems/Stochastic.jl"]
+```
+
+### Linear/Affine
+```@autodocs
+Modules = [HybridDynamics]
+Pages = ["Systems/Linear_Affine.jl"]
+```
+
+### Filippov
+```@autodocs
+Modules = [HybridDynamics]
+Pages = ["Systems/Filippov.jl"]
 ```
