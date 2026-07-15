b67ade746cb83424fab564d4049c412f7cc17594
DSmifh <davidsmith52314@gmail.com>
2026-07-15 13:09:53 -0400

    Fixed integration method testing. Looks fine to me.

--- src/ODE_Solvers/Bonus_Methods.jl
+++ src/ODE_Solvers/Bonus_Methods.jl
@@ -50,7 +50,7 @@ end
 
 #Newton Raphson solver to find roots for implicit integration steps. 
 # Solves for x in G(x) = x - c - α*h*f(x, t_new) = 0
-function implicit_newton_solve(f::Function, x_guess::Vector, c::Vector, α::AbstractFloat, h::AbstractFloat, t_new::AbstractFloat; max_iter=50, tol=1e-6)
+function implicit_newton_solve(f::Function, x_guess::Vector, c::Vector, α::AbstractFloat, h::AbstractFloat, t_new::AbstractFloat; max_iter=50, tol=1e-12)
     x_curr = copy(x_guess)
     for _ in 1:max_iter
         #Eval vector field at curent guess for z_{n+1}
