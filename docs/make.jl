eb25cf84f4896fd606f9f7ab5a8143b56275c7bb
camdenkilton-dev <camdenkilton@icloud.com>
2026-07-21 13:52:12 -0400

    test doc deployment

--- docs/make.jl
+++ docs/make.jl
@@ -39,5 +39,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/wiclark/HybridDynamics.jl"
+    repo = "github.com/wiclark/HybridDynamics.jl",
+    devbranch = "dev"
 )
